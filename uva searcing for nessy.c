7493f27a8bf38766858a35acadb8f54bb5be64d4
Kazal Chanra Barman <kcb.brurcse42@gmail.com>
2015-10-14 13:11:03 +0600

    Update uva searcing for nessy.c

--- uva searcing for nessy.c
+++ uva searcing for nessy.c
@@ -1,13 +1,26 @@
+/**********************************************************
+*  Status:
+*  Problem: uva searcing for nessy
+*  Author:  Kazal Chandra Barman,
+*           Department of Computer Sciemce and Engineering,
+*           Begum Rokeya University, Rangpur, Bangladesh
+*  Created on:
+***********************************************************/
+
+
+
 #include<stdio.h>
 #include<string.h>
 
-int main(){
-int t,m,n;
-scanf("%d",&t);
-while(t--){
- scanf("%d %d",&m,&n);
-  printf("%d\n",(m/3)*(n/3));
-}
-return 0;
+int main()
+{
+    int t,m,n;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d %d",&m,&n);
+        printf("%d\n",(m/3)*(n/3));
+    }
+    return 0;
 
 }
